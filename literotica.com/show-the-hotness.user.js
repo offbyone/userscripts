1b6a51efdaafd434b48ce0460823df30869bdef7
Chris Rose <offby1@offby1.net>
2014-04-27 13:26:59 -0700

    Chrome extension to toggle only hot stories on literotica

--- literotica.com/show-the-hotness.user.js
+++ literotica.com/show-the-hotness.user.js
@@ -6,66 +6,96 @@
 // @include        http://*.literotica.com/stories/new_submissions.php*
 // @include        https://literotica.com/stories/new_submissions.php*
 // @include        https://*.literotica.com/stories/new_submissions.php*
-// @require        http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // ==/UserScript==
 
 /* perform the actions on the completed document */
-function handler () {
+function tag_elements () {
     var storyElements = document.evaluate("//body/table/tbody/tr[5]/td",document,null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);
     
     var storyTable = storyElements.snapshotItem(0);
     
     var i = storyTable.childNodes.length - 1;
+    if (storyTable.childNodes[i].localName.toLowerCase() == 'b') {
+        i--;  // this is stripping out a trailing '</b>' that the table has
+    }
     // : checkpoint on <br> tags
     // : initial state:  title line, delete
     var mode = "title_line";
     var action = "delete";
     var ridx = 0;
     var removes = new Array();
-    while (i >= 0) {
-	cn = storyTable.childNodes[i]; i--;
-        // GM_log(" ++ Node " + i + " [ " + cn.localName + " ] -> " + mode + ", " + action);
-	
-	if (mode == "author_line") {
-	    // GM_log("++ At node " + i + ": [" + cn.localName + "] -> " + mode + ", " + action);
-	    if (cn.localName && cn.localName.toLowerCase() == "br") { mode = "title_line"; }
-	    if (action == "delete") {
-		removes[ridx++] = cn;
-	    }
-	    else {
-		// : action is retain
-	    }
-	}
-	else {
-	    // GM_log("-- At node " + i + ": [" + cn.localName + "] -> " + mode + ", " + action);
-	    // : mode is title line
-	    if (cn.localName && cn.localName.toLowerCase() == "br") { 
-		mode = "author_line";
-		// : check to see if there's an IMG tag coming up, two from here
-		if (i > 1) {
-		    inode = storyTable.childNodes[i];
-		    // GM_log("-- checking node " + (i) + ": " + inode.localName + " to see if it's an image.")
-		    if (inode.localName.toLowerCase() == "img") {
-			action = "retain";
-		    }
-		    else {
-			action = "delete";
-		    }
-		}
-	    }
-	    if (action == "delete") {
-		removes[ridx++] = cn;
-	    }
-	    else {
-		// : action is retain
-	    }
-	}
-    };
+    var group = new Array();
+    var groups = new Array();
 
-    for (var i = removes.length - 1; i >= 0; i--){
-	storyTable.removeChild(removes[i]);
-    };
+    Array.prototype.forEach.call(storyTable.childNodes, function(element, index, array) {
+        var loop_mode = mode;
+        group.push(element);
+        if (element.localName && element.localName.toLowerCase() == "br") { 
+            mode = mode == 'title_line' ? "author_line" : 'title_line'; 
+            if (loop_mode == 'author_line') {
+                // we're closing off a story.
+                groups.push(group);
+	        console.log("++ At node " + index + ": [" + element.localName + "] : " + loop_mode + '->' + mode +  "(" + group.length + " items)");
+                group = new Array();
+            }
+            else {
+	        console.log("++ At node " + index + ": [" + element.localName + "] : " + loop_mode + '->' + mode);
+            }
+        }
+    });
+
+    groups.forEach(function(element, index, array) {
+        var title = 'no title';
+        if (element.length > 2) {
+            title = element[0].textContent;
+            var story_class = null;
+            if (element[element.length - 2].localName.toLowerCase() == 'img') {
+                story_class = 'story hot';
+            }
+            else {
+                story_class = 'story not-hot';
+            }
+            jQuery(element.slice(0, element.length - 1)).wrapAll('<div class="' + story_class + '"/>');
+            element[element.length - 1].className = element[element.length - 1].className + story_class;
+        }
+        console.log("Group " + index + ", title found to be " + title + ", element count= " + element.length);
+    });
     
 }
 
-$(document).ready(handler);
+function add_button() {
+    var parent = jQuery('tr:has(td:contains("New Stories"))')[1];
+    container = parent.cells[0];
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    jQuery('<input />', { 
+        'type': 'checkbox', 
+        'id': 'show-the-hotness', 
+        'value': 'show-the-hotness' 
+    }).appendTo(container);
+    jQuery('<label />', { 
+        'id': 'story-header',
+        'class': 'story-header',
+        'for': 'show-the-hotness', 
+        'text': 'Hot New Stories' 
+    }).appendTo(container);
+
+    tag_elements();
+
+    jQuery("#show-the-hotness").change(function() {
+        var $this = jQuery(this);
+        if ($this.is(':checked')) {
+            jQuery(".story.not-hot").hide();
+            jQuery("#story-header").text('Hot New Stories');
+        } else {
+            jQuery(".story.not-hot").show();
+            jQuery("#story-header").text('All New Stories');
+        }
+    });
+
+    jQuery("#show-the-hotness").prop('checked', true);
+    jQuery("#show-the-hotness").change();
+}
+
+jQuery(document).ready(add_button);
